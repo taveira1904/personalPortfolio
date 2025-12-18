af2dba1f65d55a0fe284364745f8cd45720deff7
Tomas Ferreira <tomas.pinheiro.ferreira@gmail.com>
2025-12-18 03:35:49 +0000

    Footer update - Portfolio React

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import '../styles/Footer.css';
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>© 2025 Tomás Ferreira.</p>
+      <p>© 2025 Tomás Ferreira</p>
     </footer>
   );
 };
